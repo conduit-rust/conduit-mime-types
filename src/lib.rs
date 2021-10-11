46ef88bd2a1b03660bfd9e3b2d2375421e7029f8
Tobias Bieniek <tobias@bieniek.cloud>
2021-10-11 23:43:51 +0200

    Use `phf` crate to generate lookup maps at build-time

--- src/lib.rs
+++ src/lib.rs
@@ -1,38 +1,21 @@
-mod data;
-
-use std::collections::HashMap;
 use std::path::Path;
 
+include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
+
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct Types {
-    pub(crate) ext_by_type: HashMap<&'static str, Vec<&'static str>>,
-    type_by_ext: HashMap<&'static str, &'static str>,
-}
+pub struct Types {}
 
 impl Types {
     pub fn new() -> Result<Types, ()> {
-        let mut by_type = HashMap::new();
-        let mut by_ext = HashMap::new();
-        for (mime_type, exts) in data::MIME_TYPES {
-            by_type.insert(*mime_type, exts.iter().map(|ext| *ext).collect());
-
-            for ext in exts.iter() {
-                by_ext.insert(*ext, *mime_type);
-            }
-        }
-
-        Ok(Types {
-            ext_by_type: by_type,
-            type_by_ext: by_ext,
-        })
+        Ok(Types {})
     }
 
     pub fn get_extension(&self, name: &str) -> Option<&[&str]> {
-        self.ext_by_type.get(name).map(|v| &v[..])
+        EXT_BY_TYPE.get(name).copied()
     }
 
     pub fn get_mime_type(&self, ext: &str) -> Option<&str> {
-        self.type_by_ext.get(ext).map(|mime_type| *mime_type)
+        TYPE_BY_EXT.get(ext).copied()
     }
 
     pub fn mime_for_path(&self, path: &Path) -> &str {
@@ -46,24 +29,7 @@ impl Types {
 #[cfg(test)]
 mod test {
     use crate::Types;
-    use serde::Deserialize;
-    use std::collections::BTreeMap;
     use std::path::Path;
-
-    static JSON: &str = include_str!("../data/mime.json");
-
-    #[test]
-    fn test_against_json_data() {
-        let mut deserializer = serde_json::Deserializer::from_str(JSON);
-        let deserialized: BTreeMap<String, Vec<String>> =
-            Deserialize::deserialize(&mut deserializer).unwrap();
-
-        let t = Types::new().ok().expect("Types didn't load");
-        for (mime_type, exts) in deserialized {
-            let bundled_exts = t.get_extension(&mime_type).unwrap();
-            assert_eq!(bundled_exts, exts);
-        }
-    }
 
     #[test]
     fn test_by_ext() {
