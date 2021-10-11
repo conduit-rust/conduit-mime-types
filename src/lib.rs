028f9d1d8289e1421ae2961ceec8a296da70e548
Tobias Bieniek <tobias@bieniek.cloud>
2021-10-11 23:44:47 +0200

    Remove obsolete `Types` struct
    We don't need this to hold on to state anymore since everything is done at build-time

--- src/lib.rs
+++ src/lib.rs
@@ -2,61 +2,48 @@ use std::path::Path;
 
 include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
-#[derive(Debug, Clone, Eq, PartialEq)]
-pub struct Types {}
+pub fn get_extension(name: &str) -> Option<&[&str]> {
+    EXT_BY_TYPE.get(name).copied()
+}
 
-impl Types {
-    pub fn new() -> Result<Types, ()> {
-        Ok(Types {})
-    }
+pub fn get_mime_type(ext: &str) -> Option<&str> {
+    TYPE_BY_EXT.get(ext).copied()
+}
 
-    pub fn get_extension(&self, name: &str) -> Option<&[&str]> {
-        EXT_BY_TYPE.get(name).copied()
-    }
-
-    pub fn get_mime_type(&self, ext: &str) -> Option<&str> {
-        TYPE_BY_EXT.get(ext).copied()
-    }
-
-    pub fn mime_for_path(&self, path: &Path) -> &str {
-        path.extension()
-            .and_then(|s| s.to_str())
-            .and_then(|ext| self.get_mime_type(ext))
-            .unwrap_or_else(|| "text/plain")
-    }
+pub fn mime_for_path(path: &Path) -> &str {
+    path.extension()
+        .and_then(|s| s.to_str())
+        .and_then(|ext| get_mime_type(ext))
+        .unwrap_or_else(|| "text/plain")
 }
 
 #[cfg(test)]
 mod test {
-    use crate::Types;
+    use crate::{get_extension, get_mime_type, mime_for_path};
     use std::path::Path;
 
     #[test]
     fn test_by_ext() {
-        let t = Types::new().ok().expect("Types didn't load");
-        assert_eq!(t.get_extension("text/css").unwrap(), ["css".to_string()]);
+        assert_eq!(get_extension("text/css").unwrap(), ["css"]);
     }
 
     #[test]
     fn test_by_type() {
-        let t = Types::new().ok().expect("Types didn't load");
-        assert_eq!(t.get_mime_type("css"), Some("text/css"));
+        assert_eq!(get_mime_type("css"), Some("text/css"));
     }
 
     #[test]
     fn test_by_path() {
-        let t = Types::new().ok().expect("Types didn't load");
-
-        test_path(&t, "foo", "text/plain");
-        test_path(&t, "/path/to/foo", "text/plain");
-        test_path(&t, "foo.css", "text/css");
-        test_path(&t, "/path/to/foo.css", "text/css");
-        test_path(&t, "foo.html.css", "text/css");
-        test_path(&t, "/path/to/foo.html.css", "text/css");
-        test_path(&t, "/path/to.html/foo.css", "text/css");
+        test_path("foo", "text/plain");
+        test_path("/path/to/foo", "text/plain");
+        test_path("foo.css", "text/css");
+        test_path("/path/to/foo.css", "text/css");
+        test_path("foo.html.css", "text/css");
+        test_path("/path/to/foo.html.css", "text/css");
+        test_path("/path/to.html/foo.css", "text/css");
     }
 
-    fn test_path(types: &Types, path: &str, expected: &str) {
-        assert_eq!(types.mime_for_path(Path::new(path)), expected);
+    fn test_path(path: &str, expected: &str) {
+        assert_eq!(mime_for_path(Path::new(path)), expected);
     }
 }
